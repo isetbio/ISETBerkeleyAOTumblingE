ad80fbfad2aa3665050ea4aa0c3bc91f199f5086
David Brainard <brainard@psych.upenn.edu>
2026-03-17 07:10:37 -0400

    Started README

--- README.m
+++ README.m
@@ -10,13 +10,26 @@
 % This will also have the data in the form that the figures use to plot it,
 % so you can snag that manually if desired.  Use 'Calcs6' and match
 % nReplications to the number you computed for.
+%
+% TODO: Save plots for paper.  Save out tabular data.
 
 % Run BerkeleyAOTumblingECalcsVisualize to produce some movies of the
 % stimulus.
+%
+% TODO: Check that this still runs and document what it produces.
 
 % Run PlotTIR for a plot of the temporal impulse response.
+%
+% TODO: Check that this still runs and document what it produces.
 
-% See the configuration directory for dependencies in ToolboxToolbox json
+% Run validation/ieValidateISETBerkeleyAOTumblingE to run some basic
+% validations on the operation of the code.  Checks that the examples
+% provided in core routine BerkeleyAOTumblingEThreshold run without
+% crashing, and in one case that it produces the same answer that it did
+% when we developed the code. These passed in March 2026.  You will need
+% the full ISETBio suite setup for this, including isetvalidate.
+
+% See the configuration subdirectory for dependencies in ToolboxToolbox json
 % format, and a local hook template that shows the preferences you need to
 % set up.  See github.com/toolboxhub/toolboxtoolbox.git for the
 % ToolboxToolbox.
